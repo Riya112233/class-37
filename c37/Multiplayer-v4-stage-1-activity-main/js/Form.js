55f32df7385249cd381c83edaa4ad19645da495f
Riya112233 <114806633+Riya112233@users.noreply.github.com>
2023-01-22 15:04:45 +0530

    Add files via upload

---
+++ c37/Multiplayer-v4-stage-1-activity-main/js/Form.js
@@ -0,0 +1,9 @@
+class Form {
+  constructor() {
+    this.input=createInput("").attribute("placeholder","Enter your name")
+    this.playButton=createButton("play")
+    this.titleImg=createImg("./assets/title.png")
+    this.greeting=createElement("h2")
+  }
+
+}
